00906849fbe8c64ab5a4da8354dbe2ea6c65ffd9
Ruslan <r1554899@gmail.com>
2024-02-14 12:49:37 -0800

    Fix donation link in Hero section

--- components/sections/1 - Hero.jsx
+++ components/sections/1 - Hero.jsx
@@ -33,7 +33,7 @@ export default async function Hero() {
                 Donate to AF
               </a>
               <a href="#" className=" inline-flex items-center justify-center px-4 py-2.5 font-semibold text-sm rounded-lg text-white hover:bg-white/10 hover:border-white/10 transition bg-[#7268E7]/15 border border-white/15 backdrop-blur-xl">
-                Donate to Action Fund
+                Donate to Action Fund 
               </a>
             </div>
           </div>
