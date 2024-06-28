db7350569d430ec52044f71b1e94ecf26339c0ce
Ruslan <r1554899@gmail.com>
2024-06-27 17:06:30 -0700

    Open graph

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,27 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export const metadata = {
   title: "Washington Youth Alliance",
   description: "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
-};
+  openGraph: {
+    title: 'Washington Youth Alliance',
+    description: "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
+    siteName: 'Washington Youth Alliance',
+    images: [
+      {
+        url: '/openGraph.png',
+        width: 1600,
+        height: 900,
+        alt: 'An image of the white house with the line made with the Washington Youth Alliance colors.',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
+
 
 export default function RootLayout({ children }) {
   return (
