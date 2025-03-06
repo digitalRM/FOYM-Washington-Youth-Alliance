f62e1b315c5f0e08e9804ee195f8728e874f5145
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2025-03-06 00:34:46 -0800

    Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,44 @@
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 export const metadata = {
-  metadataBase: new URL('https://washington-youth.vercel.app'),
+  metadataBase: new URL("https://washington-youth.vercel.app"),
   title: "Washington Youth Alliance",
-  description: "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
+  description:
+    "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
   openGraph: {
-    title: 'Washington Youth Alliance',
-    description: "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
-    url: 'https://washington-youth.vercel.app',
-    siteName: 'Washington Youth Alliance',
+    title: "Washington Youth Alliance",
+    description:
+      "The Washington Youth Alliance is a youth-led organization that advocates for youth issues in Washington State.",
+    url: "https://washington-youth.vercel.app",
+    siteName: "Washington Youth Alliance",
     images: [
       {
-        url: '/openGraph.png',
+        url: "/openGraph.png",
         width: 1600,
         height: 900,
-        alt: 'An image of the white house with the line made with the Washington Youth Alliance colors.',
+        alt: "An image of the white house with the line made with the Washington Youth Alliance colors.",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
-}
-
-
-
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{background: '#f5f5f5', overflowX: 'hidden'}}><div className=" scroll-smooth">{children}</div></body>
+      <body
+        className={inter.className}
+        style={{ background: "#f5f5f5", overflowX: "hidden" }}
+      >
+        <div className=" scroll-smooth">{children}</div>
+      </body>
+      <GoogleAnalytics gaId="G-56K4X35618" />
     </html>
   );
 }
